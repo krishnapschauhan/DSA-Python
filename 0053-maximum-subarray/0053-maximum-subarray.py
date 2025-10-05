b8e0d9ded7a74ab9faf47a5ee027fb026318bdb2
Krishna Pratap Singh Chauhan <145362216+krishnapschauhan@users.noreply.github.com>
2025-10-05 10:51:26 +0530

    Time: 87 ms (48.81%), Space: 21.9 MB (33.69%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,7 +4,7 @@ class Solution(object):
         current_sum = 0
 
         for num in nums:
-            current_sum = max(num, current_sum + num)
-            max_sum = max(max_sum, current_sum)       
+            current_sum = max(num, current_sum + num)  
+            max_sum = max(max_sum, current_sum)        
 
         return max_sum
